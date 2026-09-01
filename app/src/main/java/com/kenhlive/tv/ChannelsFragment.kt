09cd7e0731fa8219f64791f97eb0862691446c04
builder <app@kenhlive.tv>
2026-09-01 16:53:31 +0700

    fix v1.4: import Dispatchers trong ChannelsFragment

--- app/src/main/java/com/kenhlive/tv/ChannelsFragment.kt
+++ app/src/main/java/com/kenhlive/tv/ChannelsFragment.kt
@@ -14,6 +14,7 @@ import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
