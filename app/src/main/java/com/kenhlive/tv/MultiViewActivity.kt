70e31756489435a0dc914583a6f9aa30b5d9b7bc
hermes <hermes@local>
2026-09-06 10:22:37 +0700

    fix compile: this trong lifecycleScope.launch bi shadow -> this@MultiViewActivity (pitfall da biet, van dinhd)

--- app/src/main/java/com/kenhlive/tv/MultiViewActivity.kt
+++ app/src/main/java/com/kenhlive/tv/MultiViewActivity.kt
@@ -111,7 +111,7 @@ class MultiViewActivity : AppCompatActivity() {
             s.player?.release(); s.fx.detach()
             s.player = ExoPlayer.Builder(this@MultiViewActivity)
                 .setTrackSelector(Enhancer.buildTrackSelector(this@MultiViewActivity))
-                .setLoadControl(Enhancer.buildLoadControl(this))
+                .setLoadControl(Enhancer.buildLoadControl(this@MultiViewActivity))
                 .build().apply {
                     setAudioAttributes(
                         AudioAttributes.Builder().setUsage(C.USAGE_MEDIA)
